3f1f2a3f7bc1296540a23ee2e3261f3c61c76a4c
Sseve <Sseve@github.com>
2024-08-03 15:36:41 +0800

    ..

--- zipappdemo/src/zipappdemo/__init__.py
+++ zipappdemo/src/zipappdemo/__init__.py
@@ -1,18 +1,22 @@
+import os
 from flask import Flask
 from jinja2 import PackageLoader, ChoiceLoader
+from werkzeug.middleware.shared_data import SharedDataMiddleware
 from zipappdemo import api
 from zipappdemo.config import Setting
 
 
 def create_app():
 
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='static')
     app.config.from_object(Setting)
 
     # 使用PackageLoader加载模板
     # 它能够从压缩包中加载模板文件
     package_loader = PackageLoader(__name__, 'templates')
     app.jinja_loader = ChoiceLoader([package_loader])
+    # 共享.pyz文件外的静态资源文件
+    app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {'/static': 'static'})
 
     app.register_blueprint(api.bp)
 
--- zipappdemo/src/zipappdemo/main.py
+++ zipappdemo/src/zipappdemo/main.py
@@ -8,3 +8,7 @@ def main():
         host=app.config["HOST"],
         port=app.config["PORT"]
     )
+
+
+if __name__ == "__main__":
+    main()
