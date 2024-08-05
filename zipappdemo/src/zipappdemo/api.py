62d0b7c8f70f4e468cf7855ccb2ba7f14aeefb19
Sseve <Sseve@github.com>
2024-08-05 09:19:45 +0800

    ..

--- zipappdemo/src/zipappdemo/api.py
+++ zipappdemo/src/zipappdemo/api.py
@@ -7,3 +7,8 @@ bp = Blueprint("api", __name__, url_prefix="/api")
 @bp.route("/", methods=("GET", "POST"))
 def test():
     return render_template("index.html")
+
+
+@bp.route("/demo", methods=("GET", "POST"))
+def demo():
+    return render_template("demo/demo.html")
